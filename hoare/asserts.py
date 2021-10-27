d460a7e22617ef1421b6712d2f7d5990ad2b7f8d
ivanc <Ivan.Camus@cloudblue.com>
2021-10-27 14:47:35 +0200

    new asset asserts

--- hoare/asserts.py
+++ hoare/asserts.py
@@ -40,6 +40,21 @@ def asset_params_value_equal(request: dict, param_id: str, expected: str):
 def asset_params_value_not_equal(request: dict, param_id: str, expected: str):
     asset_params_value(request, param_id, '!=', expected)
 
+def asset_params_value_greater(request: dict, param_id: str, expected: str):
+    asset_params_value(request, param_id, '<', expected)
+
+def asset_params_value_greater_or_equal(request: dict, param_id: str, expected: str):
+    asset_params_value(request, param_id, '<=', expected)
+
+
+def asset_params_value_lesser(request: dict, param_id: str, expected: str):
+    asset_params_value(request, param_id, '>', expected)
+
+def asset_params_value_lesser_or_equal(request: dict, param_id: str, expected: str):
+    asset_params_value(request, param_id, '>=', expected)
+
+def asset_params_value_contains(request: dict, param_id: str, expected: str):
+    asset_params_value(request, param_id, 'in', expected)
 
 def tcr_status(expected: str, request: dict):
     assert request['configuration']['status'] == expected
